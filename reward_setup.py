fcaafdbdcaa020cd88cd7008eaa1ef3af13a032e
Rob Sawyer <robscottsawyer@gmail.com>
2017-04-13 12:46:38 -0400

    Added file looping arguments
    Added functionality to write out new lines and select index

--- reward_setup.py
+++ reward_setup.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 # Global variables, can be replaced with command line arguments
 INPUT_FILENAME = "Cond2_sorted.csv"
+OUTPUT_FILENAME = "Cond2_with_rewards.csv"
 
 
 # Function for creating dictionary mapping column header to index
@@ -30,11 +31,11 @@ def pull_rule_score(rule_cell, select_index = 0):
 
 
 # Replacing each of the rule score semi-colons with single value
-def replace_rule_score(line):
+def replace_rule_score(line, select_index=0):
     line_split = line.split(",")
     for e in header_indexes.keys():
          if e in score_columns:
-             line_split[header_indexes[e]] = pull_rule_score(line_split[header_indexes[e]],select_index=0)
+             line_split[header_indexes[e]] = pull_rule_score(line_split[header_indexes[e]],select_index=select_index)
     return ",".join(line_split)
 
 
@@ -68,17 +69,21 @@ def remove_excess_commas(line):
 # Data Initialization
 data_file = open(INPUT_FILENAME, "r")
 header_line = data_file.readline()
+output_file = open(OUTPUT_FILENAME, "w")
 
 header_indexes = index_dictionary(header_line)
 score_columns = get_score_cols(header_line)
+#output_file.write(header_line)
 
 
-#for line in data_file:
-for i in range(10):
-    line = data_file.readline()
+for line in data_file:
+    #line = data_file.readline()
     line = remove_excess_commas(line)
-    line = replace_rule_score(line=line)
+    line = replace_rule_score(line=line, select_index=0)
     line = reward_function(line=line)
-    print line.split(",")[header_indexes["reward"]]
+    # output_file.write(line)
+
+data_file.close()
+output_file.close()
 
 
